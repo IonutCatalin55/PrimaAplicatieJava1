fb8dc525bd89c8b86bc7ede49eeaefdda6bc5d37
Alexandru199205 <bacescualexandru7@gmail.com>
2021-09-04 00:17:30 +0300

    mai nou

--- src/ro/java/ctrln/Salut.java
+++ src/ro/java/ctrln/Salut.java
@@ -4,6 +4,5 @@ public class Salut {
     public static void main(String[] args) {
         System.out.println("salut");
         System.out.println("De ce dra nu vrei sa maergi");
-
-    }
+        System.out.println("ddfdfdd");
 }
