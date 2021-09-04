ef521302b24fa2941efc40bcc66f7f46d27bcca6
Alexandru199205 <bacescualexandru7@gmail.com>
2021-09-04 03:08:42 +0300

    Adaugare comentariu

--- src/ro/java/ctrln/Salut.java
+++ src/ro/java/ctrln/Salut.java
@@ -1,6 +1,8 @@
 package ro.java.ctrln;
 
 public class Salut {
+    // ce cacat de treaba
+    // este asta;
     public static void main(String[] args) {
         System.out.println("salut");
         System.out.println("De ce dra nu vrei sa maergi");
