f3257d4c79a0eecde169d4185278f75b0a133641
Alexandru199205 <bacescualexandru7gmail.com>
2022-02-13 11:50:02 +0200

    adaugare comentarii multi linie

--- src/ro/java/ctrln/Salut.java
+++ src/ro/java/ctrln/Salut.java
@@ -3,6 +3,10 @@ package ro.java.ctrln;
 public class Salut {
     // ce cacat de treaba
     // este asta;
+    /*
+    acum ori da ori ba
+    kekklkkkfkkcckldkk
+     */
     public static void main(String[] args) {
         System.out.println("salut");
         System.out.println("De ce dra nu vrei sa maergi");
